8f450963337af95e37a1d13b3ac5746655375a82
Scopel Nicolas Ignacio <146841821+NachoSco@users.noreply.github.com>
2024-06-19 20:15:01 -0300

    Add files via upload

---
+++ prueba/prints.h
@@ -0,0 +1,39 @@
+#ifndef PRINTS_H
+#define PRINTS_H
+
+#include <stdio.h>
+
+// Color ANSI definitions
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_YELLOW  "\x1b[33m"
+#define ANSI_COLOR_ORANGE  "\x1b[38;5;214m"
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
+
+// Function declarations
+void print_menu();
+void print_pikachu();
+void print_dragonair();
+void print_caterpie();
+void print_digglet();
+void print_dragonite();
+void print_ekkans();
+void print_geodude();
+void print_grimer();
+void print_meowth();
+void print_pidgey();
+void print_ratata();
+void print_moltres();
+void print_zapdos();
+void print_articuno();
+void print_vulpix();
+void print_sandshrew();
+void print_raichu();
+void print_venusaur();
+void print_bulbasaur();
+void print_charmander();
+void print_squirtle();
+void print_wartortle();
+void print_blastoise();
+
+#endif // PRINTS_H
